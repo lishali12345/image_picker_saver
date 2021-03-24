9ea01365e958f70cb737e2267443718a79c6eb44
helihua <helihua@inke.cn>
2021-03-24 20:16:33 +0800

    将image_picker_saver升级到Flutter Embedding V2

--- android/src/main/java/io/flutter/plugins/imagepickersaver/ImagePickerSaverPlugin.java
+++ android/src/main/java/io/flutter/plugins/imagepickersaver/ImagePickerSaverPlugin.java
@@ -9,26 +9,33 @@ import android.os.Environment;
 import java.io.File;
 import java.io.IOException;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.PluginRegistry;
 
 
-public class ImagePickerSaverPlugin implements MethodChannel.MethodCallHandler {
+public class ImagePickerSaverPlugin implements MethodChannel.MethodCallHandler, FlutterPlugin, ActivityAware {
     private static final String CHANNEL = "plugins.flutter.io/image_picker_saver";
 
     private static final int SOURCE_CAMERA = 0;
     private static final int SOURCE_GALLERY = 1;
 
-    private final PluginRegistry.Registrar registrar;
-    private final ImagePickerDelegate delegate;
+    private PluginRegistry.Registrar registrar;
+    private ImagePickerDelegate delegate;
+    private MethodChannel channel;
+    private ActivityPluginBinding activityPluginBinding;
 
     public static void registerWith(PluginRegistry.Registrar registrar) {
         final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL);
 
         final File externalFilesDirectory =
-                registrar.activity().getExternalFilesDir(Environment. DIRECTORY_DCIM);
+                registrar.activity().getExternalFilesDir(Environment.DIRECTORY_DCIM);
         final ExifDataCopier exifDataCopier = new ExifDataCopier();
         final ImageResizer imageResizer = new ImageResizer(externalFilesDirectory, exifDataCopier);
 
@@ -39,6 +46,9 @@ public class ImagePickerSaverPlugin implements MethodChannel.MethodCallHandler {
 
         final ImagePickerSaverPlugin instance = new ImagePickerSaverPlugin(registrar, delegate);
         channel.setMethodCallHandler(instance);
+    }
+
+    public ImagePickerSaverPlugin() {
     }
 
     @VisibleForTesting
@@ -90,6 +100,61 @@ public class ImagePickerSaverPlugin implements MethodChannel.MethodCallHandler {
 
         } else {
             throw new IllegalArgumentException("Unknown method " + call.method);
+        }
+    }
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), CHANNEL);
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        activityPluginBinding = binding;
+        final File externalFilesDirectory =
+                binding.getActivity().getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        final ExifDataCopier exifDataCopier = new ExifDataCopier();
+        final ImageResizer imageResizer = new ImageResizer(externalFilesDirectory, exifDataCopier);
+
+        delegate =
+                new ImagePickerDelegate(binding.getActivity(), externalFilesDirectory, imageResizer);
+        binding.addActivityResultListener(delegate);
+        binding.addRequestPermissionsResultListener(delegate);
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+        if (activityPluginBinding != null) {
+            activityPluginBinding.removeActivityResultListener(delegate);
+            activityPluginBinding.removeRequestPermissionsResultListener(delegate);
+        }
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        activityPluginBinding = binding;
+        final File externalFilesDirectory =
+                binding.getActivity().getExternalFilesDir(Environment.DIRECTORY_DCIM);
+        final ExifDataCopier exifDataCopier = new ExifDataCopier();
+        final ImageResizer imageResizer = new ImageResizer(externalFilesDirectory, exifDataCopier);
+
+        delegate =
+                new ImagePickerDelegate(binding.getActivity(), externalFilesDirectory, imageResizer);
+        binding.addActivityResultListener(delegate);
+        binding.addRequestPermissionsResultListener(delegate);
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+        if (activityPluginBinding != null) {
+            activityPluginBinding.removeActivityResultListener(delegate);
+            activityPluginBinding.removeRequestPermissionsResultListener(delegate);
         }
     }
 }
